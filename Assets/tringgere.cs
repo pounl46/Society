c870a87c4b8b5581b427af67268ff35f2f08bd55
hyunwooo616 <nohyeonu20@gmail.com>
2026-09-07 10:02:11 +0900

    feat

---
+++ Assets/tringgere.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class tringgere : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene("Ending");
+    }
+}
